72f994f86525c2ba58013288a67e2dc4c40405ae
wangshuo <752796691@qq.com>
2022-06-12 21:21:35 +0800

    fix: delete build file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangshuo/myCode/CPlusToolLibrary/thread_pool
-# Build directory: /home/wangshuo/myCode/CPlusToolLibrary/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
