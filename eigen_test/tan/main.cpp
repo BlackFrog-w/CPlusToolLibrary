7ba3ee1531fc15ba3ac9720e685e3aef5c66d815
wangshuo <wangshuo@pudutech.com>
2022-06-24 20:55:26 +0800

    add eigen test

---
+++ eigen_test/tan/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: BlackFrog
+ * @Date: 2022-06-23 21:43:22
+ * @LastEditTime: 2022-06-23 21:43:46
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
+ */
+#include <iostream>
+
+int main(int, char**) {
+
+    //atan2函数
+    //http://c.biancheng.net/ref/atan2.html
+    std::cout << "Hello, world!\n";
+}
