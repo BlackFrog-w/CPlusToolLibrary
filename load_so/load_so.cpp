294fed0988bcac5129ecb92bad5e719a33d9fe9a
wangshuo <wangshuo@pudutech.com>
2022-06-27 20:13:59 +0800

    add generate dynamic so

---
+++ load_so/load_so.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: BlackFrog
+ * @Date: 2022-06-27 14:58:37
+ * @LastEditTime: 2022-06-27 16:37:46
+ * @Description: 
+ * https://blog.51cto.com/quietmadman/1394968
+ * 
+ * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
+ */
+#include <iostream>
+
+void say_hello(){
+    std::cout << "Hello, from load_so!\n";
+}
---
+++ load_so/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: BlackFrog
+ * @Date: 2022-06-27 16:38:00
+ * @LastEditTime: 2022-06-27 16:38:04
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
+ */
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
