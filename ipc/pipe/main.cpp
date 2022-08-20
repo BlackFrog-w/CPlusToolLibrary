f43e5e6e4da2568d3400afa99ee6cf3d3fe1dbd8
wangshuo <752796691@qq.com>
2022-08-20 10:46:27 +0800

    update ipc

---
+++ ipc/pipe/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * @Author: BlackFrog
+ * @Date: 2022-08-19 20:45:03
+ * @LastEditTime: 2022-08-19 23:01:16
+ * @Description: 
+ * 进程间通信方式：管道
+ * 
+ * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
+ */
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<string.h> 
+#include <iostream> 
+
+int main(int argc, char *argv[]) 
+{ 
+	int pipefd[2]; 
+	if(pipe(pipefd) == -1) 
+		perror("pipe error!"); 
+ 
+	pid_t pid; 
+	pid = fork(); 
+	if(pid == -1) 
+		perror("fork error!"); 
+ 
+	if(pid == 0){ 
+		close(pipefd[0]); 
+		write(pipefd[1], "hellopp", 7); 
+		close(pipefd[1]); 
+		exit(EXIT_SUCCESS);
+        std::cout << "hello, wangshuo" << std::endl; 
+	} 
+ 
+	close(pipefd[1]); 
+	char buf[10] = {0}; 
+	read(pipefd[0], buf, 10); 
+	printf("buf=%s\n", buf); 
+ 
+    sleep(20);
+	return 0; 
+} 
