419e059155a9f21b9a6bd7f147e29d3dae8456cc
wangshuo <752796691@qq.com>
2022-10-17 22:40:57 +0800

    删除编译生成中间文件

--- build2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blackfrog/cplus/CPlusToolLibrary/ipc/shared_mem
-# Build directory: /home/blackfrog/cplus/CPlusToolLibrary/build2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
