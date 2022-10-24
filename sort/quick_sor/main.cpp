fd8b805c9d37abe07c7869cb45f989a5f597a991
wangshuo <752796691@qq.com>
2022-10-24 23:57:14 +0800

    update quick sort algo

---
+++ sort/quick_sor/main.cpp
@@ -0,0 +1,90 @@
+/*
+ * @Author: BlackFrog
+ * @Date: 2022-10-23 00:00:18
+ * @LastEditTime: 2022-10-24 23:45:39
+ * @Description: 
+ * 快速排序
+ * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static int num = 1;
+
+void quickSort(vector<int>& a, int low, int high) {
+	if (low >= high)		// 结束标志
+		return;
+	int first = low;		// 低位下标
+	int last = high;		// 高位下标
+	int key = a[first];		// 设第一个为基准
+
+	while (first < last)
+	{
+		// 从后往前走，将比第一个小的移到前面
+		while (first < last && a[last] > key)
+			last--;
+		if (first < last)
+			a[first++] = a[last];
+
+
+
+
+
+        if(num == 1) {
+            cout << "-------------------------------------------------------" << endl;
+            for(auto i = a.begin(); i != a.end(); ++i) {
+                cout << *i << endl;
+            }
+            ++num;
+        }
+
+
+		//从前往后走， 将比第一个大的移到后面
+		while (first < last && a[first] <= key)
+			first++;
+		if (first < last)
+			a[last--] = a[first];
+
+        if(num == 2) {
+            cout << "-------------------------------------------------------" << endl;
+            for(auto i = a.begin(); i != a.end(); ++i) {
+            cout << *i << endl;
+            }
+            ++num;
+        }
+
+        cout << "first is: " << first << ", last is: " << last << endl;    
+	}
+
+	a[first] = key;
+    if(num == 3) {
+        cout << "-------------------------------------------------------" << endl;
+        for(auto i = a.begin(); i != a.end(); ++i) {
+        cout << *i << endl;
+        }
+        ++num;
+    }  
+	// 前半递归
+	quickSort(a, low, first - 1);
+	// 后半递归
+	quickSort(a, first + 1, high);
+	
+
+
+
+}
+
+int main(int, char**) {
+
+    vector<int> v{5, 2, 6, 9, 8, 4, 1, 3, 7, 10};
+
+    quickSort(v, 0, 9);
+
+    for(auto i = v.begin(); i != v.end(); ++i) {
+        // cout << *i << endl;
+    }
+
+    std::cout << "Hello, world!\n";
+}
--- thread_async/packaged_task/main.cpp
+++ thread_async/packaged_task/main.cpp
@@ -1,13 +1,12 @@
 /*
  * @Author: BlackFrog
- * @Date: 2022-10-23 00:55:37
- * @LastEditTime: 2022-10-23 01:03:25
+ * @Date: 2022-10-23 15:11:58
+ * @LastEditTime: 2022-10-23 15:11:58
  * @Description: 
- * 参考链接：
- * https://blog.csdn.net/m0_37621078/article/details/89789943
+ * 
  * Copyright (c) 2022 by BlackFrog, All Rights Reserved. 
  */
-//future2.cpp 使用promise传递被调用线程返回结果，通过共享状态变化通知调用线程已获得结果
+//future3.cpp 使用packaged_task传递被调用线程返回结果，通过共享状态变化通知调用线程已获得结果
 
 #include <vector>
 #include <thread>
@@ -16,23 +15,20 @@
 #include <iostream>
 #include <chrono>
  
-void accumulate(std::vector<int>::iterator first,
-                std::vector<int>::iterator last,
-                std::promise<int> accumulate_promise)
+int accumulate(std::vector<int>::iterator first,
+                std::vector<int>::iterator last)
 {
-    //累加求和
     int sum = std::accumulate(first, last, 0);
-    accumulate_promise.set_value(sum);  // 将结果存入，并让共享状态变为就绪以提醒future
+    return sum;
 }
  
 int main()
 {
-    // 演示用 promise<int> 在线程间传递结果。
+    // 演示用 packaged_task 在线程间传递结果。
     std::vector<int> numbers = { 1, 2, 3, 4, 5, 6 };
-    std::promise<int> accumulate_promise;
-    std::future<int> accumulate_future = accumulate_promise.get_future();
-    std::thread work_thread(accumulate, numbers.begin(), numbers.end(),
-                            std::move(accumulate_promise));
+    std::packaged_task<int(std::vector<int>::iterator,std::vector<int>::iterator)> accumulate_task(accumulate);
+    std::future<int> accumulate_future = accumulate_task.get_future();
+    std::thread work_thread(std::move(accumulate_task), numbers.begin(), numbers.end());
     accumulate_future.wait();  //等待结果
     std::cout << "result=" << accumulate_future.get() << '\n';
     work_thread.join();  //阻塞等待线程执行完成
